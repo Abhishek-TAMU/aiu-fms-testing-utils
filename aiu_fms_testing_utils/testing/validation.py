4c94b52914324fb1dd3810d2c4cd43ab3e5b1599
Abhishek <maurya.abhishek@ibm.com>
2026-04-01 21:08:16 -0400

    Print check filename of cpu infio 2
    Signed-off-by: Abhishek <maurya.abhishek@ibm.com>

--- aiu_fms_testing_utils/testing/validation.py
+++ aiu_fms_testing_utils/testing/validation.py
@@ -155,7 +155,7 @@ def get_default_validation_prefix(
     kwargs_str = format_kwargs_to_string(**kwargs)
 
     filename = f"{kwargs_str}"
-    r0dprint("filename of CPU info: {filename}")
+    r0dprint(f"filename of CPU info: {filename}")
     hash_object = hashlib.sha256(filename.encode("utf-8"))
     hex_digest = hash_object.hexdigest()
     return f"{hex_digest}_{aftu_version}"
